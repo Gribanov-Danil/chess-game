ec9bd9ca3e5db385f9c164da02d2814b1fbfb05f
Danil Gribanov <gribanovwork@yandex.ru>
2023-03-30 01:04:47 +0500

    feat: сделал захардкоженную доску

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,8 +1,13 @@
 import React from "react"
 import "./App.scss"
+import { BoardComponent } from "../board-component/board-component"
 
 function App() {
-  return <div className="app">dfdf</div>
+  return (
+    <div className="app">
+      <BoardComponent />
+    </div>
+  )
 }
 
 export default App
---
+++ src/components/board-component/board-component.tsx
@@ -0,0 +1,38 @@
+export const BoardComponent = () => {
+  return (
+    <div className="board">
+      <div className="cell white"></div>
+      <div className="cell black"></div>
+      <div className="cell white"></div>
+      <div className="cell black"></div>
+      <div className="cell white"></div>
+      <div className="cell black"></div>
+      <div className="cell white"></div>
+      <div className="cell black"></div>
+      <div className="cell white"></div>
+      <div className="cell black"></div>
+      <div className="cell white"></div>
+      <div className="cell black"></div>
+      <div className="cell white"></div>
+      <div className="cell black"></div>
+      <div className="cell white"></div>
+      <div className="cell black"></div>
+      <div className="cell white"></div>
+      <div className="cell black"></div>
+      <div className="cell white"></div>
+      <div className="cell black"></div>
+      <div className="cell white"></div>
+      <div className="cell black"></div>
+      <div className="cell white"></div>
+      <div className="cell black"></div>
+      <div className="cell white"></div>
+      <div className="cell black"></div>
+      <div className="cell white"></div>
+      <div className="cell black"></div>
+      <div className="cell white"></div>
+      <div className="cell black"></div>
+      <div className="cell white"></div>
+      <div className="cell black"></div>
+    </div>
+  )
+}
